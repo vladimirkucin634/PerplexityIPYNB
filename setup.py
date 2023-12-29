45b3c57f32096bc47d1b35aae76ca50ecb0eb18f
vladimirkucin634 <126107125+vladimirkucin634@users.noreply.github.com>
2023-12-29 22:33:09 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name="PerplexityIPYNB",
+    version="1.0",
+    description="Perplexity AI in iPython Notebooks",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vladimirkucin634/PerplexityIPYNB",
+    author="vladimirkucin634",
+    license="MIT",
+    keywords=[
+        "artificial-intelligence",
+        "perplexityai",
+        "perplexity",
+        "python",
+        "api",
+        "ai",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages(exclude=[".github"]),
+    install_requires=[
+        "requests",
+        "websocket-client",
+    ],
+    project_urls={
+        "Source": "https://github.com/vladimirkucin634/PerplexityIPYNB",
+    },
+)
