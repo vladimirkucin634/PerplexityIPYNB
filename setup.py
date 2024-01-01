f376fde0edcce1ec8732a2eee2341e43cc281660
vladimirkucin634 <126107125+vladimirkucin634@users.noreply.github.com>
2024-01-01 13:08:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="PerplexityIPYNB",
-    version="1.0",
+    version="1.01",
     description="Perplexity AI in iPython Notebooks",
     long_description=README,
     long_description_content_type="text/markdown",
